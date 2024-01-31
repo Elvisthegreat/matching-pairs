acd7ed44cc00d46d7d64de0bdc729c921f89e683
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-01-31 00:45:09 +0000

    Corrected Errors

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,3 @@
-// document.getElementById('restart').addEventListener('click', restartGame())
-
 //images array
 let images = ["assets/images/foto1.webp",
 "assets/images/foto2.webp",
@@ -16,36 +14,39 @@ let images = ["assets/images/foto1.webp",
 let board = [];
 let rows = 4;
 let columns = 5;
+// Game state
 let firstCard = null; // store the first card clicked
 let secondCard = null; // store the second card clicked
 let matchedCards = 0; // count the number of matched cards
 
 
 window.onload = function () {
-    shuffleCards();
     startGame();
 };
 
-function shuffleCards() {
-    images = [...images, ...images]; // Duplicate the images array to have pairs of each image
-    console.log(images);
+function shuffleCards(cards) {
+    let shuffledCards = [...cards, ...cards]; // Duplicate the images array to have pairs of each image
+    console.log(shuffleCards);
     // loop over the array from the last element to the second element
-    for (let i = images.length - 1; i > 0; i--) {
+    for (let i = shuffledCards.length - 1; i > 0; i--) {
         // pick a random index from 0 to i
         let x = Math.floor(Math.random() * (i + 1));
         // swap the elements at i and x
-        let temp = images[i];
-        images[i] = images[x];
-        images[x] = temp;
+        let temp = shuffledCards[i];
+        shuffledCards[i] = shuffledCards[x];
+        shuffledCards[x] = temp;
     }
-    console.log(images);
+    console.log(shuffledCards);
+    return shuffledCards;
 }
 const startGame = () => {
+    // shuffle the cards and assign them to the board
+    let shuffledCards = shuffleCards(images);
     //Arrange the board by 4x5
     for (let row = 0; row < rows; row++) {
         let roww = [];
         for (let column = 0; column < columns; column++) {
-            let cardImg = images.pop();
+            let cardImg = shuffledCards.pop();
             roww.push(cardImg);
 
             // Create the html image tag
@@ -69,5 +70,16 @@ function frontCard(){
             let card = document.getElementById(row.toString() + "." + column.toString());
             card.src = "/assets/images/back.jpg"
         }
+    }
+}
+
+function lookForMatch(){
+     // get the images of the first and second cards
+    let firstimage = firstCard.src;
+    let secondimage = firstCard.src;
+     
+    //compare both images
+    if(firstimage === secondimage){
+
     }
 }
