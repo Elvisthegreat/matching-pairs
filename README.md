f0b74082065cb31107bcb5f3ffd3b93afaf71e22
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-05-10 18:07:26 +0000

    added some code validation images for prove that no bugs are found in my code

--- README.md
+++ README.md
@@ -4,15 +4,25 @@ Welcome yourself to a *Memory game*, also know as **Matching Pairs** - a game we
 
 ![Screenshot (375)](https://github.com/Elvisthegreat/Rock-paper-scissors/assets/141064225/e9eb6267-6501-4690-999b-ed926f62020a)
 
-MATCHING PAIRS or memory game is a delightful and challenging activity that tests your ability to recall information it’s about training your brain to retain information and make connections. Its a very simple and easy game to play with time counting downward, you flip a card then flip another one to look for the same exact image, and when you match a pair...those two matched images stay without flipping back, then you have to continue till you match all rows and column. But if you are unable to match all cards before the time runs out, a end game function is called and you won't be able to flip anymore cards, the only way to keep on playing is to click on the restart button to get all functions back in place :D. So flip those cards, exercise your mind, and enjoy the thrill of matching!
+MATCHING PAIRS or memory game is a delightful and challenging activity that tests your ability to recall information it’s about training your brain to retain information and make connections. Its a very simple and easy game to play with time counting downward, you flip a card then flip another one to look for the same exact image, and when you match a pair...those two matched images stay without flipping back, then you have to continue till you match all rows and column. But if you are unable to match all cards before the time runs out, a end game function is called and you won't be able to flip anymore cards, the only way to keep on playing is to click on the restart button to get all functions back in place :D. So flip those cards, exercise your mind, and enjoy the thrill of matching! [View live game](https://elvisthegreat.github.io/matching-pairs/)
 
 <hr>
 
 ## **TABLE OF CONTENTS**
-   [Users Goal](#users-goal)
-   [Site Owner Goal](#site-owner-goal)
-   [Users Experience](#users-experience)
-   [Users Stories](#users-stories)
+   [Users Goal](#users-goal) <br>
+   [Site Owner Goal](#site-owner-goal) <br>
+   [Users Experience](#users-experience) <br>
+   [Users Stories](#users-stories) <br>
+   [Features](#features) <br>
+   [Technologoies Used](#technologoies-used) <br>
+   [Design](#design) <br>
+   [Colors](#Colors) <br>
+   [Libraries & Tools](#libraries-&-tools) <br>
+   [Validation](#validation) <br>
+   [Testing](#testing) <br>
+   [](#) <br>
+   [](#) <br>
+   [](#) <br>
 
 ## **Users Goal**
    - To get what they expected
@@ -66,3 +76,71 @@ MATCHING PAIRS or memory game is a delightful and challenging activity that test
    10. As a site owner I should provide a game from bugs so that user can have a good user experience
    11. As a site owner I should make sure each card flip work as expected
    12. As a site owner I should make sure restart button restart every part of the game so that user can have good user experience
+
+## Features
+- **Row and Columns**: The game board consists of a row and columns that is customized to hold different numbers of cards.
+- **Timer**: Keep track of how long it takes you to complete the game.
+- **Restart Button**: Help you restart the game at any giving moment.
+![Screenshot (487)](https://github.com/Elvisthegreat/matching-pairs/assets/141064225/8552d232-aae0-4019-af96-6efdc2d83447)
+
+   
+
+## Technologies Used
+- HTML5
+- CSS3
+- JavaScript
+- jQuery
+
+## Design
+### Design Idea
+This game was designed in a way things could be easy for a user and texts are easily read, well displaying colors and ai images as the flip cards
+
+## Colors
+   ### Background-color
+   - background-color: #0b0b39;.
+   I chose #0b0b39 type blue color as the background color because i wanted something that could make my text well readable
+
+
+### Libraries & Tools
+
+- [Am I Responsive](http://ami.responsivedesign.is/)
+- [Favicon.io](https://favicon.io)
+- [Chrome dev tools](https://developers.google.com/web/tools/chrome-devtools/)
+- [Font Awesome](https://fontawesome.com/)
+- [Git](https://git-scm.com/)
+- [GitHub](https://github.com/)
+- [Google Fonts](https://fonts.google.com/)
+- [Heroku Platform](https://id.heroku.com/login)
+- [jQuery](https://jquery.com)
+
+## Validation
+  - [WC3 Validator](https://validator.w3.org/)
+     <details><summary> view validation HTML</summary>
+     <img src="https://github.com/Elvisthegreat/matching-pairs/assets/141064225/013a9c8d-60e3-45c5-befc-23c310123eaf">
+     </details>
+
+  - [Jigsaw W3 Validator](https://jigsaw.w3.org/css-validator/)
+     <details><summary>view validation CSS</summary>
+     <img src="https://github.com/Elvisthegreat/matching-pairs/assets/141064225/9c111eb7-4424-4340-8b48-a7770dd42c84">
+     </details>
+
+  - [JShint](https://jshint.com/)
+     <details><summary> view validation JShint</summary>
+      <img src="https://github.com/Elvisthegreat/matching-pairs/assets/141064225/b13b4576-99c2-46d4-935b-56e0b12ba158">
+     </details>
+
+  - [Lighthouse](https://developers.google.com/web/tools/lighthouse/)
+     <details><summary> view validation Lighthouse</summary>
+     <img src="">
+     </details>
+
+## Testing
+
+**Step** | **Expected Result** | **Actual Result**
+------------ | ------------ | ------------ |
+| Click on any cards | card will flip over| Works as expected |
+| Click on the 'Restart button' | Everything will restart back to normal| Works as expected |
+| Click on the 'Restart' | Time will restart back to 60 seconds| Works as expected |
+| Samll screen size 'Responsive' | Game is reduce to fit| Works as expected |
+| Large screen size 'Responsive'| Game height and width increase to fit| Works as expected |
+
--- assets/script.js
+++ assets/script.js
@@ -1,215 +1,216 @@
 //images array
 let images = [
-"assets/images/three-chicken.webp",
-"assets/images/cyber-street.webp",
-"assets/images/tallest-buiding-in-dubai.webp",
-"assets/images/sky-scrapper.webp",
-"assets/images/intense-cold-weather.webp",
-"assets/images/influencer.webp",
-"assets/images/children-in-school-bus.webp",
-"assets/images/chrismas-snow.webp",
-"assets/images/animal-bear.webp",
-"assets/images/man-in-space.webp"
-];
-
-/**
- * A random strings to be pick from when 
- * all cards are all matched
- */
-const congratsArray = [
-     'You are the game master',
-     'Keep rolling.',
-     'You are the man!',
-     'Excellent! Great job!',
-     'Looks like it was too easy for you! :D',
-     "Magnificent",
-     'Better days are ahead of you',
-     "You spiced it up",
-     "Should i call you a Sir? :D!",
-     'Best to ever do it alone'
-]
-
-// Declare a Restart game variable and add an EventListener to it for restart
-document.getElementById('restart').addEventListener('click', restartGame);
-
-let board = [];
-let rows = 4;
-let columns = 5;
-// Game state
-let firstCard ; // store the first card clicked
-let secondCard; // store the second card clicked
-let matchedCards = 0; // count the number of matched cards
-
-let isGameOver = false; // Global variable to prevent cards flip when game is over
-
-// For the time
-const countDownTime = document.getElementById("timer");
-let countdownInterval = null; // Store the interval
-// Initial Time
-let seconds = 61;
-
-const countDownTiming = () => {
-    seconds --;
-    // seconds logic
-    if( seconds == 0){
-        gameOver();
-    }
-
-//format time before displaying
-let secondsValue = seconds <= 61 ? `${seconds}` : seconds;
-countDownTime.innerHTML = `<span>Time:</span> ${secondsValue}`;
-};
-
-// Call the startGame immediately the window is loaded
-window.onload = function () {
-    startGame();
-};
-
-function shuffleCards(cards) {
-    let shuffledCards = [...cards, ...cards]; // Duplicate the images array to have pairs of each image
-    // loop over the array from the last element to the second element
-    for (let i = shuffledCards.length - 1; i > 0; i--) {
-        // pick a random index from 0 to i
-        let x = Math.floor(Math.random() * (i + 1));
-        // swap the elements at i and x
-        let temp = shuffledCards[i];
-        shuffledCards[i] = shuffledCards[x];
-        shuffledCards[x] = temp;
-    }
-    return shuffledCards;
-}
-const startGame = () => {
-    // shuffle the cards and assign them to the board
-    let shuffledCards = shuffleCards(images);
-    //Arrange the board by 4x5
-    for (let row = 0; row < rows; row++) {
-        let roww = [];
-        for (let column = 0; column < columns; column++) {
-            let cardImg = shuffledCards.pop();
-            roww.push(cardImg);
-
-            // Create the html image tag
-            let card = document.createElement("img");
-            card.id = row.toString() + "." + column.toString();
-            card.src = cardImg;
-            card.src = "assets/images/back.jpg";
-            card.classList.add("card");
-            card.classList.add("flip");
-            // EventListener for the clickCard
-            card.addEventListener('click', clickCard);
-            document.getElementById("board").append(card);
+    "assets/images/three-chicken.webp",
+    "assets/images/cyber-street.webp",
+    "assets/images/tallest-buiding-in-dubai.webp",
+    "assets/images/sky-scrapper.webp",
+    "assets/images/intense-cold-weather.webp",
+    "assets/images/influencer.webp",
+    "assets/images/children-in-school-bus.webp",
+    "assets/images/chrismas-snow.webp",
+    "assets/images/animal-bear.webp",
+    "assets/images/man-in-space.webp"
+    ];
+    
+    /**
+     * A random strings to be pick from when 
+     * all cards are all matched
+     */
+    const congratsArray = [
+         'You are the game master',
+         'Keep rolling.',
+         'You are the man!',
+         'Excellent! Great job!',
+         'Looks like it was too easy for you! :D',
+         "Magnificent",
+         'Better days are ahead of you',
+         "You spiced it up",
+         "Should i call you a Sir? :D!",
+         'Best to ever do it alone'
+    ];
+    
+    // Declare a Restart game variable and add an EventListener to it for restart
+    document.getElementById('restart').addEventListener('click', restartGame);
+    
+    let board = [];
+    let rows = 4;
+    let columns = 5;
+    // Game state
+    let firstCard ; // store the first card clicked
+    let secondCard; // store the second card clicked
+    let matchedCards = 0; // count the number of matched cards
+    
+    let isGameOver = false; // Global variable to prevent cards flip when game is over
+    
+    // For the time
+    const countDownTime = document.getElementById("timer");
+    let countdownInterval = null; // Store the interval
+    // Initial Time
+    let seconds = 61;
+    
+    const countDownTiming = () => {
+        seconds --;
+        // seconds logic
+        if( seconds == 0){
+            gameOver();
         }
-        board.push(roww);
-    }
-    countdownInterval = setInterval(countDownTiming, 1000); // call countDownTiming every 1000 milliseconds
-};
-
-function frontCard(){
-    for(let row = 0; row < rows; row++){
-        for(let column = 0; column < columns; column++){
-            let card = document.getElementById(row.toString() + "." + column.toString());
-            card.src = "assets/images/back.jpg"
+    
+    //format time before displaying
+    let secondsValue = seconds <= 61 ? `${seconds}` : seconds;
+    countDownTime.innerHTML = `<span>Time:</span> ${secondsValue}`;
+    };
+    
+    // Call the startGame immediately the window is loaded
+    window.onload = function () {
+        startGame();
+    };
+    
+    function shuffleCards(cards) {
+        let shuffledCards = [...cards, ...cards]; // Duplicate the images array to have pairs of each image
+        // loop over the array from the last element to the second element
+        for (let i = shuffledCards.length - 1; i > 0; i--) {
+            // pick a random index from 0 to i
+            let x = Math.floor(Math.random() * (i + 1));
+            // swap the elements at i and x
+            let temp = shuffledCards[i];
+            shuffledCards[i] = shuffledCards[x];
+            shuffledCards[x] = temp;
         }
+        return shuffledCards;
     }
-}
-
-function clickCard() {
-
-    if (!isGameOver){ // Only flip the cards if the game is not over
-
-    if (this.src.includes("back.")) {
-        if(!firstCard){
-            firstCard = this;
-            let row = firstCard.id.split('.')[0];
-            let column = this.id.split(".")[1];
-            this.src = board[row][column];
-            this.classList.add("flipped");
-            
-        }else if(!secondCard && this != firstCard){
-            secondCard = this;
-            let row = secondCard.id.split('.')[0];
-            let column = this.id.split(".")[1];
-            this.src = board[row][column] ;
-            this.classList.add("flipped");
-            lookForMatch();
+    const startGame = () => {
+        // shuffle the cards and assign them to the board
+        let shuffledCards = shuffleCards(images);
+        //Arrange the board by 4x5
+        for (let row = 0; row < rows; row++) {
+            let roww = [];
+            for (let column = 0; column < columns; column++) {
+                let cardImg = shuffledCards.pop();
+                roww.push(cardImg);
+    
+                // Create the html image tag
+                let card = document.createElement("img");
+                card.id = row.toString() + "." + column.toString();
+                card.src = cardImg;
+                card.src = "assets/images/back.jpg";
+                card.classList.add("card");
+                card.classList.add("flip");
+                // EventListener for the clickCard
+                card.addEventListener('click', clickCard);
+                document.getElementById("board").append(card);
+            }
+            board.push(roww);
+        }
+        countdownInterval = setInterval(countDownTiming, 1000); // call countDownTiming every 1000 milliseconds
+    };
+    
+    function frontCard(){
+        for(let row = 0; row < rows; row++){
+            for(let column = 0; column < columns; column++){
+                let card = document.getElementById(row.toString() + "." + column.toString());
+                card.src = "assets/images/back.jpg";
+            }
         }
     }
-  }
-}
-
-  function lookForMatch() {
-    // Compare the sources of the first and second cards
-    if (firstCard.src != secondCard.src) {
-    setTimeout( () => {  // Flip them back to the back image after a delay
-        firstCard.src = "assets/images/back.jpg";
-        secondCard.src = "assets/images/back.jpg";
-
+    
+    function clickCard() {
+    
+        if (!isGameOver){ // Only flip the cards if the game is not over
+    
+        if (this.src.includes("back.")) {
+            if(!firstCard){
+                firstCard = this;
+                let row = firstCard.id.split('.')[0];
+                let column = this.id.split(".")[1];
+                this.src = board[row][column];
+                this.classList.add("flipped");
+                
+            }else if(!secondCard && this != firstCard){
+                secondCard = this;
+                let row = secondCard.id.split('.')[0];
+                let column = this.id.split(".")[1];
+                this.src = board[row][column] ;
+                this.classList.add("flipped");
+                lookForMatch();
+            }
+        }
+      }
+    }
+    
+      function lookForMatch() {
+        // Compare the sources of the first and second cards
+        if (firstCard.src != secondCard.src) {
+        setTimeout( () => {  // Flip them back to the back image after a delay
+            firstCard.src = "assets/images/back.jpg";
+            secondCard.src = "assets/images/back.jpg";
+    
+            firstCard = null;
+            secondCard = null;
+          
+          }, 1000);
+        } else{
+          matchedCards += 2; // Increment the matchedCards count
+          firstCard = null ;
+          secondCard = null;
+        }
+    
+        // Check if all cards are matched
+        // Then call the congrats function
+        if (matchedCards === rows * columns) {
+            clearInterval(countdownInterval); // Stop the countdown if all cards are matched
+            congrats();
+        }
+    
+    }
+    
+    function restartGame() {
+        // Clear the board array
+        board = [];
+        // Reset the game state variables
         firstCard = null;
         secondCard = null;
-      
-      }, 1000);
-    } else{
-      matchedCards += 2; // Increment the matchedCards count
-      firstCard = null ;
-      secondCard = null;
+        matchedCards = 0;
+        // Reset gameOver to false when a new game starts so cards can be flip again
+        isGameOver = false;
+    
+        // Remove all the cards from the board in the DOM
+        let boardElement = document.getElementById("board");
+        while (boardElement.firstChild) {
+            boardElement.firstChild.src = "assets/images/back.jpg";
+            boardElement.removeChild(boardElement.firstChild);
+        }
+    
+         // Clear the countdown interval and reset the seconds
+         clearInterval(countdownInterval);
+         seconds = 60;
+
+    
+    /*Calling the startGame function inside the
+     restartGame function to Start a new game 
+     when the restart button is clicked*/
+    startGame();
+    } 
+    
+    function congrats() {
+        //Randomly select from the congratsArray if all cards are matched
+        let randomPick = Math.floor(Math.random() * congratsArray.length);
+         alert(congratsArray[randomPick]);
+     }
+    
+    
+    // Game over function
+    function gameOver(){
+        alert('Game Over! :D');
+        clearInterval(countdownInterval); // Stop the countdown
+        isGameOver = true;
     }
-
-    // Check if all cards are matched
-    // Then call the congrats function
-    if (matchedCards === rows * columns) {
-        clearInterval(countdownInterval); // Stop the countdown if all cards are matched
-        congrats();
-    }
-
-}
-
-function restartGame() {
-    // Clear the board array
-    board = [];
-    // Reset the game state variables
-    firstCard = null;
-    secondCard = null;
-    matchedCards = 0;
-    // Reset gameOver to false when a new game starts so cards can be flip again
-    isGameOver = false;
-
-    // Remove all the cards from the board in the DOM
-    let boardElement = document.getElementById("board");
-    while (boardElement.firstChild) {
-        boardElement.firstChild.src = "assets/images/back.jpg";
-        boardElement.removeChild(boardElement.firstChild);
-    }
-
-     // Clear the countdown interval and reset the seconds
-     clearInterval(countdownInterval);
-     seconds = 60;
-
-/*Calling the startGame function inside the
- restartGame function to Start a new game 
- when the restart button is clicked*/
-startGame();
-} 
-
-function congrats() {
-    //Randomly select from the congratsArray if all cards are matched
-    let randomPick = Math.floor(Math.random() * congratsArray.length);
-     alert(congratsArray[randomPick]);
- }
-
-
-// Game over function
-function gameOver(){
-    alert('Game Over! :D');
-    clearInterval(countdownInterval); // Stop the countdown
-    isGameOver = true;
-}
-
-
-// jQuery for the Restart button color effect
-$('.restart1').mouseenter(function(){
-    $(this).addClass('turn-blue').removeClass('restart1')
-});
-
-$('.restart1').mouseleave(function(){
-    $(this).removeClass('turn-blue').addClass('restart1')
-});
+    
+    
+    // jQuery for the Restart button color effect
+    $('.restart1').mouseenter(function(){
+        $(this).addClass('turn-blue').removeClass('restart1');
+    });
+    
+    $('.restart1').mouseleave(function(){
+        $(this).removeClass('turn-blue').addClass('restart1');
+    });
